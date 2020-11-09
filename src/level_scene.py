b05ce0c055f68bf6522a66a7c6140152626ed8ee
jeremycryan <jeremychristopherryan@gmail.com>
2020-11-08 19:00:54 -0600

    Fix crash on ship deletion

--- src/level_scene.py
+++ src/level_scene.py
@@ -14,12 +14,12 @@ class LevelScene(Scene):
                         Planet(self.game, (500, 500), 0, 50),
                         Moon(self.game, (800, 300))]
         self.ships = [Ship(self.game, "r90 t33 d200; t0 d500; r0 t33 d2000; r360 d260; r0 t33 d500; t0", self.game.players["Paul"], (500, 200), 180),
-                      Ship(self.game, "t50", self.game.players["Jeremy"], (500, 200, 180))]
+                      Ship(self.game, "t50", self.game.players["Jeremy"], (500, 200), 180)]
 
     def update(self, dt, events):
         for ship in self.ships[::-1]:
             if ship.destroyed:
-                self.ship.remove(ship)
+                self.ships.remove(ship)
         for object_to_update in self.ships + self.planets:
             object_to_update.update(dt, events)
 
